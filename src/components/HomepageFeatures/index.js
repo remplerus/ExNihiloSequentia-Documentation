c44fcd488cb0eec955dd98a9c63abb3ba23a6b64
Jacob Williams <jacob.t.williams@outlook.com>
2022-07-08 15:06:57 -0500

    Add project icons and placeholders

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import CrookIcon from '@site/static/img/exn_crook_icon.png';
+import HammerIcon from '@site/static/img/exn_hammer_icon.png';
 
 const FeatureList = [
   {
     title: 'Ex Nihilo: Sequentia',
+    img: CrookIcon,
     description: (
       <>
         Ex Nihilo: Sequentia is a modern port of Ex Nihilo: Creatio for 1.12, which in turn was a fork of Ex Nihilo: Adscensio for 1.10, which was a continuation of Ex Nihilo for 1.7.10.
@@ -13,6 +16,7 @@ const FeatureList = [
   },
   {
     title: 'Ex Nihilo: Sequentia - AE Addon',
+    img: CrookIcon,
     description: (
       <>
         Addon that adds all neccessary items for compatibility with Applied Energistics 2.
@@ -21,6 +25,7 @@ const FeatureList = [
   },
   {
     title: 'Ex Nihilo: Sequentia - Mekanism Addon',
+    img: CrookIcon,
     description: (
       <>
         Addon that adds all neccessary items for compatibility with Mekanism.
@@ -29,6 +34,7 @@ const FeatureList = [
   },
   {
     title: 'Ex Nihilo: Sequentia - Tinkers Addon',
+    img: HammerIcon,
     description: (
       <>
         Addon that adds all neccessary items for compatibility with Tinkers Construct.
@@ -37,6 +43,7 @@ const FeatureList = [
   },
   {
     title: 'Ex Nihilo: Sequentia - Thermal Addon',
+    img: CrookIcon,
     description: (
       <>
         Addon that adds all neccessary items for compatibility with the Thermal Series.
@@ -45,14 +52,17 @@ const FeatureList = [
   },
 ];
 
-function Feature({ title, description }) {
+function Feature({ title, img, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
+        <div style={{ display: "flex" }}>
+          <img src={img} height={64} />
+          <h3>{title}</h3>
+        </div>
+      <p>{description}</p>
     </div>
+    </div >
   );
 }
 
